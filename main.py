6fd35b515a9bdb1a663a7682a37ce8f35337385f
unknown <500075823@stu.upes.ac.in>
2021-06-27 02:40:09 +0530

    Basic RPi Cam

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ Created on Sun Jun 27 01:47:04 2021
 @author: Neel
 """
 
+from picamera import PiCamera
+from time import sleep
 
+live = PiCamera()
+
+live.start_preview()
+sleep(5)
+live.stop_preview()
