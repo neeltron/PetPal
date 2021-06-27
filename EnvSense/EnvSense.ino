7412056b55a97f4de87b19a5234ce20c38904524
unknown <500075823@stu.upes.ac.in>
2021-06-27 12:23:24 +0530

    Added Servo

--- EnvSense/EnvSense.ino
+++ EnvSense/EnvSense.ino
@@ -1,11 +1,18 @@
 #include<dht11.h>
+#include <Servo.h>
 dht11 DHT;
+Servo S1;
 
 void setup() {
   Serial.begin(9600);
+  S1.attach(3);
 }
 
 void loop() {
   int chk = DHT.read(2);
   Serial.println(DHT.temperature);
+  S1.write(90);
+  delay(1000);
+  S1.write(0);
+  delay(1000);
 }
